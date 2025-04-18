5375376cef74db407b2b9ed7d6f537dab7c90f1d
haennes <hannes.hofmuth@gmail.com>
2025-04-18 16:23:24 +0200

    fmt

--- module.nix
+++ module.nix
@@ -1,4 +1,9 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   cfg = config.services.fs-watcher;
   inherit (lib)
@@ -33,23 +38,24 @@ in
       type = userNameType;
       description = ''The username under which to run the services by default'';
     };
-              recursive = mkOption {
-                type = bool;
-                description = ''Wether to watch the directories recursively'';
-                default = true;
-              };
+    recursive = mkOption {
+      type = bool;
+      description = ''Wether to watch the directories recursively'';
+      default = true;
+    };
     directories = mkOption {
       type = attrsOf (
         submodule (
           { name, ... }:
           let
-          innerCfg = cfg.directories.${name};
-          regexOpt = description:
-            mkOption {
-              type = nullOr str;
-              default = null;
-              description = "${description} matching the extended regular expression";
-            };
+            innerCfg = cfg.directories.${name};
+            regexOpt =
+              description:
+              mkOption {
+                type = nullOr str;
+                default = null;
+                description = "${description} matching the extended regular expression";
+              };
           in
           {
             options = {
@@ -89,48 +95,108 @@ in
                 default = null;
               };
               notifications =
-              mapListToAttrs(set:
-              {
+                mapListToAttrs
+                  (set: {
 
-                inherit (set) name;
-                value = {
-                  enable = mkOption {
-                    type = bool;
-                    inherit (set) default;
-                    description = ''
-                      wether to watch for changes of kind:
-                      ${set.description}
-                    '';
-                  };
-                  command = mkOption {
-                    type = str;
-                    default = innerCfg.command;
-                    description = ''
-                    The command to execute on change:
-                    ${set.description}
-                    Defaults to the one defined one level above
-                    '';
-                  };
+                    inherit (set) name;
+                    value = {
+                      enable = mkOption {
+                        type = bool;
+                        inherit (set) default;
+                        description = ''
+                          wether to watch for changes of kind:
+                          ${set.description}
+                        '';
+                      };
+                      command = mkOption {
+                        type = str;
+                        default = innerCfg.command;
+                        description = ''
+                          The command to execute on change:
+                          ${set.description}
+                          Defaults to the one defined one level above
+                        '';
+                      };
 
-                };
-              })
-              [
-                { name = "access"; description= "file or directory contents were read"; default = false;}
-	        { name = "modify"; description= "file or directory contents were written"; default = true;}
-	        { name = "attrib"; description= "file or directory attributes changed"; default = false;}
-	        { name = "close_write"; description= "file or directory closed, after being opened in writable mode"; default = true;}
-	        { name = "close_nowrite"; description="file or directory closed, after being opened in read-only mode"; default = false;}
-	        { name = "close"; description="file or directory closed, regardless of read/write mode"; default = false;}
-	        { name = "open"; description="file or directory opened"; default = false;}
-	        { name = "moved_to"; description="file or directory moved to watched directory"; default = true;}
-	        { name = "moved_from"; description="file or directory moved from watched directory"; default = false;}
-	        { name = "move"; description="file or directory moved to or from watched directory "; default = false;}
-                { name = "move_self"; description="A watched file or directory was moved."; default = false;}
-	        { name = "create"; description="file or directory created within watched directory"; default = true;}
-	        { name = "delete"; description="file or directory deleted within watched directory"; default = false;}
-	        { name = "delete_self"; description="file or directory was deleted"; default = false;}
-	        { name = "unmount"; description="file system containing file or directory unmounted"; default = false;}
-              ];
+                    };
+                  })
+                  [
+                    {
+                      name = "access";
+                      description = "file or directory contents were read";
+                      default = false;
+                    }
+                    {
+                      name = "modify";
+                      description = "file or directory contents were written";
+                      default = true;
+                    }
+                    {
+                      name = "attrib";
+                      description = "file or directory attributes changed";
+                      default = false;
+                    }
+                    {
+                      name = "close_write";
+                      description = "file or directory closed, after being opened in writable mode";
+                      default = true;
+                    }
+                    {
+                      name = "close_nowrite";
+                      description = "file or directory closed, after being opened in read-only mode";
+                      default = false;
+                    }
+                    {
+                      name = "close";
+                      description = "file or directory closed, regardless of read/write mode";
+                      default = false;
+                    }
+                    {
+                      name = "open";
+                      description = "file or directory opened";
+                      default = false;
+                    }
+                    {
+                      name = "moved_to";
+                      description = "file or directory moved to watched directory";
+                      default = true;
+                    }
+                    {
+                      name = "moved_from";
+                      description = "file or directory moved from watched directory";
+                      default = false;
+                    }
+                    {
+                      name = "move";
+                      description = "file or directory moved to or from watched directory ";
+                      default = false;
+                    }
+                    {
+                      name = "move_self";
+                      description = "A watched file or directory was moved.";
+                      default = false;
+                    }
+                    {
+                      name = "create";
+                      description = "file or directory created within watched directory";
+                      default = true;
+                    }
+                    {
+                      name = "delete";
+                      description = "file or directory deleted within watched directory";
+                      default = false;
+                    }
+                    {
+                      name = "delete_self";
+                      description = "file or directory was deleted";
+                      default = false;
+                    }
+                    {
+                      name = "unmount";
+                      description = "file system containing file or directory unmounted";
+                      default = false;
+                    }
+                  ];
             };
           }
         )
@@ -138,51 +204,68 @@ in
     };
   };
   config =
-  let
-    commandScript = {command, notifications, ifOutputOlder, match, name, ...}:
     let
-    eventsOpts = map(name: "-e ${name}") (attrNames (filterAttrs(_: set: set.enable) notifications));
-    cut = col: ''$(echo -ne "$line" | cut -d $'\0' -f ${toString col})'';
-    commandFmt = "${command} $dir $file $dir_file $time";
-    finalCommand = if ifOutputOlder != null then
-    ''
-      output_name=$(${ifOutputOlder} $dir $file $dir_file $time)
-      tmpdir=$(mktemp -d)
-      echo "$output_name: $dir_file" >> $tmpdir/Makefile
-      echo "    ${command} "$dir" "$file" "$dir" "$dif_file" "$time"" >> $tmpdir/Makefile
-      ${pkgs.gnumake} -f $tmpdir/Makefile $output_name
-      echo "Makefile created in $tmpdir"
-      #rm -rf $tmpdir
-    ''
-    else commandFmt;
-    in pkgs.writeShellScript name ''
-      echo "file updated: $1" > "/tmp/fs-watcher-logs"
-      ${pkgs.inotify-tools}/bin/inotifywait -m ${concatStringsSep " " eventsOpts} ${optionalString (match.include != null) "--include ${match.include}"} ${optionalString (match.includei != null) "--includei ${match.includei}"} ${optionalString (match.exclude != null) "--exclude ${match.exclude}"} ${optionalString (match.excludei != null) "--excludei ${match.excludei}"} --format '%w%0%f%0%w%f%0%e%0%T' | while IFS= read -r -d \'\' line; do
-        export dir=${cut 1}
-        export file=${cut 2}
-        export dir_file=${cut 3}
-        export event=${cut 4}
-        export time=${cut 5}
-        pushd $dir
-          ${finalCommand}
-        popd
-        done
-    '';
-  in
-  {
-    systemd.services = lib.mkIf cfg.enable (
-      mapAttrs' (name: value: {
-        name = "watcher-${replaceStrings [ "/" ] [ "-" ] name}";
-        value = {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "local-fs.target" ];
-          serviceConfig = {
-            Type = "notify";
-            User = value.user;
-            ExecStart = "${commandScript (value // {inherit name;})}";
+      commandScript =
+        {
+          command,
+          notifications,
+          ifOutputOlder,
+          match,
+          name,
+          ...
+        }:
+        let
+          eventsOpts = map (name: "-e ${name}") (attrNames (filterAttrs (_: set: set.enable) notifications));
+          cut = col: ''$(echo -ne "$line" | cut -d $'\0' -f ${toString col})'';
+          commandFmt = "${command} $dir $file $dir_file $time";
+          finalCommand =
+            if ifOutputOlder != null then
+              ''
+                output_name=$(${ifOutputOlder} $dir $file $dir_file $time)
+                tmpdir=$(mktemp -d)
+                echo "$output_name: $dir_file" >> $tmpdir/Makefile
+                echo "    ${command} "$dir" "$file" "$dir" "$dif_file" "$time"" >> $tmpdir/Makefile
+                ${pkgs.gnumake} -f $tmpdir/Makefile $output_name
+                echo "Makefile created in $tmpdir"
+                #rm -rf $tmpdir
+              ''
+            else
+              commandFmt;
+        in
+        pkgs.writeShellScript name ''
+          echo "file updated: $1" > "/tmp/fs-watcher-logs"
+          ${pkgs.inotify-tools}/bin/inotifywait -m ${concatStringsSep " " eventsOpts} ${
+            optionalString (match.include != null) "--include ${match.include}"
+          } ${optionalString (match.includei != null) "--includei ${match.includei}"} ${
+            optionalString (match.exclude != null) "--exclude ${match.exclude}"
+          } ${
+            optionalString (match.excludei != null) "--excludei ${match.excludei}"
+          } --format '%w%0%f%0%w%f%0%e%0%T' | while IFS= read -r -d \'\' line; do
+            export dir=${cut 1}
+            export file=${cut 2}
+            export dir_file=${cut 3}
+            export event=${cut 4}
+            export time=${cut 5}
+            pushd $dir
+              ${finalCommand}
+            popd
+            done
+        '';
+    in
+    {
+      systemd.services = lib.mkIf cfg.enable (
+        mapAttrs' (name: value: {
+          name = "watcher-${replaceStrings [ "/" ] [ "-" ] name}";
+          value = {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "local-fs.target" ];
+            serviceConfig = {
+              Type = "notify";
+              User = value.user;
+              ExecStart = "${commandScript (value // { inherit name; })}";
+            };
           };
-        };
-      }) cfg.directories
-    );
-  };
+        }) cfg.directories
+      );
+    };
 }
